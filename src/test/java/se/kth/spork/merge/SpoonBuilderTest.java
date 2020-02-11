650c7fe6043ba1da6d53573beb463f350fe3d82f
Simon Larsén <slarse@kth.se>
2020-02-11 21:28:19 +0100

    Add experimental GumTree to Spoon parser/interpreter

---
+++ src/test/java/se/kth/spork/merge/SpoonBuilderTest.java
@@ -0,0 +1,37 @@
+package se.kth.spork.merge;
+
+import com.github.gumtreediff.tree.ITree;
+import gumtree.spoon.builder.SpoonGumTreeBuilder;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import spoon.Launcher;
+import spoon.reflect.declaration.CtClass;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SpoonBuilderTest {
+    private static final Path testDirpath = Paths.get("src/test/resources/standalone");
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Adder", "Sum"})
+    void buildTree_shouldBuildCorrectSpoonTree(String testName) throws IOException {
+        String fileName = testName + ".java";
+        String fileContents = read(testDirpath.resolve(fileName));
+        CtClass<?> initial = Launcher.parseClass(fileContents);
+
+        ITree gumTree = new SpoonGumTreeBuilder().getTree(initial);
+
+        CtClass<?> rebuilt = new SpoonBuilder().buildSpoonTree(gumTree);
+
+        assertEquals(initial.toString(), rebuilt.toString());
+    }
+
+    private static String read(Path path) throws IOException {
+        return String.join("\n", Files.readAllLines(path));
+    }
+}
---
+++ src/test/resources/standalone/Adder.java
@@ -0,0 +1,6 @@
+public class Adder {
+    public int add(int a, int b) {
+        // this method is great
+        return a + b;
+    }
+}
