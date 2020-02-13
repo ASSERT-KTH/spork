c7fc57ed9e003ea1f801f01171f0d1cbf69a5034
Simon Larsén <slarse@kth.se>
2020-02-13 08:52:52 +0100

    Refactor test suite setup

--- src/test/java/se/kth/spork/merge/TdmMergeTest.java
+++ src/test/java/se/kth/spork/merge/TdmMergeTest.java
@@ -18,32 +18,39 @@ import static org.junit.jupiter.api.Assertions.*;
 class TdmMergeTest {
     private static final Path cleanMergeDirpath = Paths.get("src/test/resources/clean");
     private static final Path leftModifiedDirpath = cleanMergeDirpath.resolve("left_modified");
-    private static final Path rightModifiedDirpath = cleanMergeDirpath.resolve("right_modified");
     private static final Path bothModifiedDirpath = cleanMergeDirpath.resolve("both_modified");
 
     @ParameterizedTest
     @ValueSource(strings = {"add_parameter"})
     void mergeToTree_shouldReturnExpectedTree_whenLeftVersionIsModified(String testName) throws IOException{
         File testDir = leftModifiedDirpath.resolve(testName).toFile();
-        runTestMerge(testDir);
+        TestSources sources = getTestSources(testDir);
+        runTestMerge(sources);
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"add_parameter"})
     void mergeToTree_shouldReturnExpectedTree_whenRightVersionIsModified(String testName) throws IOException{
-        File testDir = rightModifiedDirpath.resolve(testName).toFile();
-        runTestMerge(testDir);
+        File testDir = leftModifiedDirpath.resolve(testName).toFile();
+        TestSources sources = getTestSources(testDir);
+
+        // swap left and right around to make this a "right modified" test case
+        String left = sources.left;
+        sources.left = sources.right;
+        sources.right = left;
+
+        runTestMerge(sources);
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"move_if"})
     void mergeToTree_shouldReturnExpectedTree_whenBothVersionsAreModified(String testName) throws IOException {
         File testDir = bothModifiedDirpath.resolve(testName).toFile();
-        runTestMerge(testDir);
+        TestSources sources = getTestSources(testDir);
+        runTestMerge(sources);
     }
 
-    private static void runTestMerge(File testDir) throws IOException {
-        TestSources sources = getTestSources(testDir);
+    private static void runTestMerge(TestSources sources) throws IOException {
         ITree expected = toGumTree(sources.expected);
 
         CtClass<?> base = Launcher.parseClass(sources.base);
