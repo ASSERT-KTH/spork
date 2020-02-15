dfd6872cf0b98f1b45a4bda766b33a0cffb06dbb
Simon Larsén <slarse@kth.se>
2020-02-15 15:18:46 +0100

    Add more test cases

---
+++ src/test/resources/clean/both_modified/add_parameter/Left.java
@@ -0,0 +1,9 @@
+public class Adder {
+    public int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    public int sub(int a, int b) {
+        return a - b;
+    }
+}
---
+++ src/test/resources/clean/left_modified/add_if_block/Expected.java
@@ -0,0 +1,8 @@
+public class Adder {
+    public int add(int a, int b) {
+        if (a < b) {
+            return b + a;
+        }
+        return a + b;
+    }
+}
