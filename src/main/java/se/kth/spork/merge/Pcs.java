4eec79033d0493210e6f51ec2a703206371f7967
Simon Larsén <slarse@kth.se>
2020-02-16 11:18:42 +0100

    Clean up and document the Pcs class

--- src/main/java/se/kth/spork/merge/Pcs.java
+++ src/main/java/se/kth/spork/merge/Pcs.java
@@ -1,38 +1,13 @@
 package se.kth.spork.merge;
 
-/*
- * This file is part of GumTree.
- *
- * GumTree is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * GumTree is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with GumTree.  If not, see <http://www.gnu.org/licenses/>.
- *
- * Copyright 2016 Jean-Rémy Falleri <jr.falleri@gmail.com>
- * Copyright 2016 Floréal Morandat <florealm@gmail.com>
- */
-
-import com.github.gumtreediff.tree.ITree;
-
-import java.util.HashSet;
 import java.util.Objects;
-import java.util.Set;
 import java.util.function.Function;
 
 /**
- * This file is adapted from GumTree, see the original license statement above. The only non-trivial thing that
- * is borrowed is the fromTree method, everything else is either trivial or original.
+ * Representation of a Parent/Child/Successor triple for 3DM merge. Note that the revision does not (and should not)
+ * impact hashing or equality, it is just there as metainformation for resolving and flagging conflicts.
  *
- * Representation of a Parent/Child/Successor triple. Note that the revision does not (and should not) impact hashing
- * or equality, it is just there as metainformation.
+ * @author Simon Larsén
  */
 public class Pcs<T> {
     private T root;
@@ -42,28 +17,33 @@ public class Pcs<T> {
     private Function<T, String> toStr;
     private Function<T, Integer> hash;
 
+    /**
+     * @param root The root of this PCS.
+     * @param predecessor The predecessor (or child) of this PCS.
+     * @param successor The successor of this PCS.
+     */
     public Pcs(T root, T predecessor, T successor) {
         this(root, predecessor, successor, T::toString, T::hashCode);
     }
 
+    /**
+     * This constructor allows for setting of custom toString and hashCode functions to be used on the root, predecessor
+     * and successor when computing the hash and string representation of the entire PCS. This may be desirable if the
+     * default implementations of T (i.e. the type used in the PCS) are not what you want.
+     *
+     * @param root The root of this PCS.
+     * @param predecessor The predecessor (or child) of this PCS.
+     * @param successor The successor of this PCS.
+     * @param toStr A custom toString method.
+     * @param hash A custom hashCode method.
+     */
     public Pcs(T root, T predecessor, T successor, Function<T, String> toStr, Function<T, Integer> hash) {
         this.root = root;
         this.predecessor = predecessor;
         this.successor = successor;
-        revision = null;
         this.toStr = toStr;
         this.hash = hash;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Pcs pcs = (Pcs) o;
-        return Objects.equals(root, pcs.root)
-                && Objects.equals(predecessor, pcs.predecessor)
-                && Objects.equals(successor, pcs.successor);
+        revision = null;
     }
 
     @Override
@@ -100,10 +80,6 @@ public class Pcs<T> {
         this.revision = revision;
     }
 
-    private int applyHash(T elem) {
-        return elem == null ? 0 : hash.apply(elem);
-    }
-
     @Override
     public int hashCode() {
         int rootHash = applyHash(root);
@@ -112,4 +88,18 @@ public class Pcs<T> {
         return Objects.hash(rootHash, predecessorHash, successorHash);
     }
 
+    private int applyHash(T elem) {
+        return elem == null ? 0 : hash.apply(elem);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Pcs<?> pcs = (Pcs<?>) o;
+        return Objects.equals(root, pcs.root)
+                && Objects.equals(predecessor, pcs.predecessor)
+                && Objects.equals(successor, pcs.successor);
+    }
 }
