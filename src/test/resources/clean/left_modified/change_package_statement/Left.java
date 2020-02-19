26fabbf0e79127a70ca5e798b31a082fa9473b3d
Simon Larsén <slarse@kth.se>
2020-02-19 16:13:00 +0100

    [misc] Add missing test resources

---
+++ src/test/resources/clean/left_modified/change_package_statement/Left.java
@@ -0,0 +1,7 @@
+package totally.different.pkg.statement;
+
+public class Adder {
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/test/resources/clean/left_modified/change_package_statement/Right.java
@@ -0,0 +1,7 @@
+package se.kth.spork;
+
+public class Adder {
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
