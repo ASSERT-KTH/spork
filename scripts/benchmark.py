6ed4949f54b0ffe28939ae203958ee67ca1b9b73
Simon Larsén <slarse@kth.se>
2020-03-05 18:12:59 +0100

    Update benchmark script

--- scripts/benchmark.py
+++ scripts/benchmark.py
@@ -117,25 +117,98 @@ def run_merge(scenario_dir, merge_cmd, compare_cmd):
         return True
 
 
+def merge_files_separately(merge_scenarios, merge_cmd, cmp_cmd):
+    num_passed = 0
+    num_failed = 0
+
+    for merge in merge_scenarios:
+        diff_left = merge.base.diff(merge.left)
+        diff_right = merge.base.diff(merge.right)
+        diff_result = merge.base.diff(merge.result)
+
+        mapping = collections.defaultdict(dict)
+        insert("left", diff_left, mapping)
+        insert("right", diff_right, mapping)
+        insert("result", diff_result, mapping)
+
+        # if merge.result.hexsha == "08ebba7998647c258437132baadad922c8b43419":
+        #    continue
+
+        print(f"running merge scenarios for commit {merge.result.hexsha}")
+
+        start = time.time_ns()
+        with tempfile.TemporaryDirectory() as tmpdir:
+            merge_base_dir = pathlib.Path(tmpdir)
+            create_merge_dirs(merge_base_dir, merge.result.hexsha, mapping)
+            for merge_dir in merge_base_dir.iterdir():
+                assert merge_dir.is_dir()
+
+                for scenario_dir in merge_dir.iterdir():
+                    assert scenario_dir.is_dir()
+                    if run_merge(scenario_dir, merge_cmd, cmp_cmd):
+                        num_passed += 1
+                    else:
+                        num_failed += 1
+
+        end = time.time_ns()
+        delta = (end - start) / 1e9
+        print(f"passed: {num_passed}, failed: {num_failed}")
+        print(f"Time elapsed: {delta} seconds")
+
+
+def run_git_merge(merge_scenarios: List[MergeScenario], repo: git.Repo):
+    passed_merge = 0
+    failed_merge = 0
+
+    passed_build = 0
+    failed_build = 0
+
+    for mc in merge_scenarios:
+        print(f"MERGING {mc}\n\n")
+        repo.git.checkout(mc.left.hexsha, "-f")
+
+        try:
+            out = repo.git.merge(mc.right.hexsha)
+            print("MERGE OK")
+            passed_merge += 1
+
+        except git.GitCommandError as exc:
+            print(exc)
+            failed_merge += 1
+
+    print(f"passed_merge: {passed_merge}, failed_merge: {failed_merge}")
+    print(f"passed_build: {passed_build}, failed_build: {failed_build}")
+
+
+def build_with_maven(repo: git.Repo) -> bool:
+    proc = subprocess.run("mvn clean compile -U".split(), cwd=repo.working_dir)
+    return proc.returncode == 0
+
+
+def build_with_gradle(repo: git.Repo) -> bool:
+    print("BUILDING WITH GRADLE")
+    proc = subprocess.run(
+        f"./gradlew clean build -x test -x testng".split(), cwd=repo.working_dir
+    )
+    return proc.returncode == 0
+
+
 def create_cli_parser():
     parser = argparse.ArgumentParser(
         "Spork merge tester", description="A little program to help develop Spork!"
     )
 
-    parser.add_argument(
-        "--merge-cmd", help="Merge command.", type=str, required=True,
-    )
-    parser.add_argument(
-        "--cmp-cmd", help="Compare command.", type=str, required=True,
-    )
-    parser.add_argument(
+    base_parser = argparse.ArgumentParser(add_help=False)
+
+    base_parser.add_argument(
         "-r",
         "--repo",
-        help="Name of the repo to run tests on",
+        help="Name of the repo to run tests on. If -g is not specified, repo "
+        "is assumed to be local.",
         type=str,
         required=True,
     )
-    parser.add_argument(
+    base_parser.add_argument(
         "-g",
         "--github-user",
         help="GitHub username to fetch repo from. Is combined with `--repo`"
@@ -143,12 +216,32 @@ def create_cli_parser():
         "not provided, the repo argument is assumend to be a local directory.",
         type=str,
     )
-    parser.add_argument(
+    base_parser.add_argument(
         "-n",
         "--num-merges",
         help="Maximum amount of merges to recreate.",
         type=int,
         default=100,
+    )
+
+    subparsers = parser.add_subparsers(dest="command")
+    subparsers.required = True
+
+    test_command = subparsers.add_parser(
+        "test",
+        help="Test a merge tool by merging one file at a time.",
+        parents=[base_parser],
+    )
+
+    test_command.add_argument(
+        "--merge-cmd", help="Merge command.", type=str, required=True,
+    )
+    test_command.add_argument(
+        "--cmp-cmd", help="Compare command.", type=str, required=True,
+    )
+
+    evaluate_command = subparsers.add_parser(
+        "evaluate", help="Evaluate a merge tool by configuring git-merge to use it.", parents=[base_parser],
     )
 
     return parser
@@ -169,46 +262,18 @@ def main():
     else:
         repo = git.Repo(args.repo)
 
-    merge_scenarios = extract_merge_scenarios(repo)[: args.num_merges]
-
-    num_passed = 0
-    num_failed = 0
+    merge_scenarios = extract_merge_scenarios(repo)
+    if args.num_merges > 0:
+        merge_scenarios = merge_scenarios[: args.num_merges]
 
     print(f"recreating {len(merge_scenarios)} merges")
 
-    for merge in merge_scenarios[:50]:
-        diff_left = merge.base.diff(merge.left)
-        diff_right = merge.base.diff(merge.right)
-        diff_result = merge.base.diff(merge.result)
-
-        mapping = collections.defaultdict(dict)
-        insert("left", diff_left, mapping)
-        insert("right", diff_right, mapping)
-        insert("result", diff_result, mapping)
-
-        #if merge.result.hexsha == "08ebba7998647c258437132baadad922c8b43419":
-        #    continue
-
-        print(f"running merge scenarios for commit {merge.result.hexsha}")
-
-        start = time.time_ns()
-        with tempfile.TemporaryDirectory() as tmpdir:
-            merge_base_dir = pathlib.Path(tmpdir)
-            create_merge_dirs(merge_base_dir, merge.result.hexsha, mapping)
-            for merge_dir in merge_base_dir.iterdir():
-                assert merge_dir.is_dir()
-
-                for scenario_dir in merge_dir.iterdir():
-                    assert scenario_dir.is_dir()
-                    if run_merge(scenario_dir, args.merge_cmd, args.cmp_cmd):
-                        num_passed += 1
-                    else:
-                        num_failed += 1
-
-        end = time.time_ns()
-        delta = (end - start) / 1e9
-        print(f"passed: {num_passed}, failed: {num_failed}")
-        print(f"Time elapsed: {delta} seconds")
+    if args.command == "test":
+        merge_files_separately(merge_scenarios, args.merge_cmd, args.cmp_cmd)
+    elif args.command == "evaluate":
+        run_git_merge(merge_scenarios, repo)
+    else:
+        raise ValueError(f"Unexpected command: {args.command}")
 
 
 if __name__ == "__main__":
