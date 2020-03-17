2e0438fe7a5d06aa22a40cb7ceb145d489ba3a05
Simon Larsén <slarse@kth.se>
2020-03-17 09:22:39 +0100

    [misc] Add license statement to OperatorHelper class #69

--- src/main/java/se/kth/spork/cli/OperatorHelper.java
+++ src/main/java/se/kth/spork/cli/OperatorHelper.java
@@ -1,7 +1,23 @@
-/**
- * Copyright (C) 2006-2019 INRIA and contributors
+/*
+ * This file is taken from the Spoon library, see <a href="https://github.com/inria/spoon">Spoon's GitHub page</a> for
+ * more info on it.
+ */
+
+/*
+ * Copyright (C) INRIA and contributors
  *
- * Spoon is available either under the terms of the MIT License (see LICENSE-MIT.txt) of the Cecill-C License (see LICENSE-CECILL-C.txt). You as the user are entitled to choose the terms under which to adopt Spoon.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 package se.kth.spork.cli;
 
