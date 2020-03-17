b5a5ee199d3ad3637de21c8dbde9e879fdcb0f2f
Simon Larsén <slarse@kth.se>
2020-03-17 08:43:08 +0100

    [misc] Vendor OperatorHelper from Spoon #69

---
+++ src/main/java/se/kth/spork/cli/OperatorHelper.java
@@ -0,0 +1,102 @@
+/**
+ * Copyright (C) 2006-2019 INRIA and contributors
+ *
+ * Spoon is available either under the terms of the MIT License (see LICENSE-MIT.txt) of the Cecill-C License (see LICENSE-CECILL-C.txt). You as the user are entitled to choose the terms under which to adopt Spoon.
+ */
+package se.kth.spork.cli;
+
+import spoon.SpoonException;
+import spoon.reflect.code.BinaryOperatorKind;
+import spoon.reflect.code.UnaryOperatorKind;
+
+/**
+ * Computes source code representation of the operator
+ */
+class OperatorHelper {
+
+    private OperatorHelper() {
+    }
+
+    public static boolean isPrefixOperator(UnaryOperatorKind o) {
+        return isSufixOperator(o) == false;
+    }
+    public static boolean isSufixOperator(UnaryOperatorKind o) {
+        return o.name().startsWith("POST");
+    }
+
+    /**
+     * @return java source code representation of a pre or post unary operator.
+     */
+    public static String getOperatorText(UnaryOperatorKind o) {
+        switch (o) {
+            case POS:
+                return "+";
+            case NEG:
+                return "-";
+            case NOT:
+                return "!";
+            case COMPL:
+                return "~";
+            case PREINC:
+                return "++";
+            case PREDEC:
+                return "--";
+            case POSTINC:
+                return "++";
+            case POSTDEC:
+                return "--";
+            default:
+                throw new SpoonException("Unsupported operator " + o.name());
+        }
+    }
+
+    /**
+     * @return java source code representation of a binary operator.
+     */
+    public static String getOperatorText(BinaryOperatorKind o) {
+        switch (o) {
+            case OR:
+                return "||";
+            case AND:
+                return "&&";
+            case BITOR:
+                return "|";
+            case BITXOR:
+                return "^";
+            case BITAND:
+                return "&";
+            case EQ:
+                return "==";
+            case NE:
+                return "!=";
+            case LT:
+                return "<";
+            case GT:
+                return ">";
+            case LE:
+                return "<=";
+            case GE:
+                return ">=";
+            case SL:
+                return "<<";
+            case SR:
+                return ">>";
+            case USR:
+                return ">>>";
+            case PLUS:
+                return "+";
+            case MINUS:
+                return "-";
+            case MUL:
+                return "*";
+            case DIV:
+                return "/";
+            case MOD:
+                return "%";
+            case INSTANCEOF:
+                return "instanceof";
+            default:
+                throw new SpoonException("Unsupported operator " + o.name());
+        }
+    }
+}
