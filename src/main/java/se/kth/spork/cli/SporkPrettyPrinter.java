0d0c429fe73ce3036b05747d5392f4d6688482cb
Simon Larsén <slarse@kth.se>
2020-04-09 15:51:49 +0200

    Refactor pretty printer

--- src/main/java/se/kth/spork/cli/SporkPrettyPrinter.java
+++ src/main/java/se/kth/spork/cli/SporkPrettyPrinter.java
@@ -5,25 +5,19 @@ import se.kth.spork.spoon.PcsInterpreter;
 import se.kth.spork.spoon.StructuralConflict;
 import se.kth.spork.util.Pair;
 import spoon.compiler.Environment;
-import spoon.reflect.code.CtBlock;
 import spoon.reflect.code.CtComment;
-import spoon.reflect.cu.SourcePosition;
 import spoon.reflect.declaration.CtElement;
 import spoon.reflect.declaration.CtField;
 import spoon.reflect.declaration.CtMethod;
-import spoon.reflect.declaration.CtType;
 import spoon.reflect.visitor.DefaultJavaPrettyPrinter;
 import spoon.reflect.visitor.DefaultTokenWriter;
 import spoon.reflect.visitor.PrinterHelper;
 import spoon.reflect.visitor.PrintingContext;
-import spoon.support.StandardEnvironment;
-import spoon.support.sniper.SniperJavaPrettyPrinter;
 
-import java.io.IOException;
-import java.io.RandomAccessFile;
-import java.nio.charset.Charset;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Map;
+import java.util.Optional;
 
 public final class SporkPrettyPrinter extends DefaultJavaPrettyPrinter {
     public static final String START_CONFLICT = "<<<<<<< LEFT";
@@ -71,13 +65,8 @@ public final class SporkPrettyPrinter extends DefaultJavaPrettyPrinter {
         } else if (e.getMetadata(PcsInterpreter.SINGLE_REVISION_KEY) != null &&
                 (e instanceof CtMethod || e instanceof CtField)) {
             CtElement originalElement = (CtElement) e.getMetadata(PcsInterpreter.ORIGINAL_NODE_KEY);
-            String sniped = getOriginalSource(originalElement);
-            /**
-            env.useTabulations(true); // required for sniper mode
-            String sniped = originalElement.toString();
-            env.useTabulations(false); // required for SporkPrettyPrinter
-             */
-            printerHelper.writeSniperContent(sniped, getTabCount(originalElement));
+            String originalSource = SourceExtractor.getOriginalSource(originalElement);
+            printerHelper.writeRawSourceCode(originalSource, SourceExtractor.getIndentation(originalElement));
             return this;
         }
 
@@ -116,132 +105,9 @@ public final class SporkPrettyPrinter extends DefaultJavaPrettyPrinter {
      * Write both pats of a structural conflict.
      */
     private void writeStructuralConflict(StructuralConflict structuralConflict) {
-        String leftSource = getOriginalSource(structuralConflict.left);
-        String rightSource = getOriginalSource(structuralConflict.right);
+        String leftSource = SourceExtractor.getOriginalSource(structuralConflict.left);
+        String rightSource = SourceExtractor.getOriginalSource(structuralConflict.right);
         printerHelper.writeConflict(leftSource, rightSource);
-    }
-
-    /**
-     * Write the provided string plus a line ending only if the string is non-empty.
-     */
-    private void writelnNonEmpty(String s) {
-        if (s.isEmpty())
-            return;
-
-        PrinterHelper helper = getPrinterTokenWriter().getPrinterHelper();
-
-        helper.write(s);
-        helper.writeln();
-    }
-
-    /**
-     * Get the original source fragment corresponding to the nodes provided, or an empty string if the list is
-     * empty. Note that the nodes must be adjacent in the source file, and in the same order as in the source.
-     *
-     * @param nodes A possibly empty list of adjacent nodes.
-     * @return The original source code fragment, including any leading indentation on the first line.
-     */
-    private static String getOriginalSource(List<CtElement> nodes) {
-        if (nodes.isEmpty())
-            return "";
-
-        SourcePosition firstElemPos = getSourcePos(nodes.get(0));
-        SourcePosition lastElemPos = getSourcePos(nodes.get(nodes.size() - 1));
-        return getOriginalSource(firstElemPos, lastElemPos);
-    }
-
-    private static String getOriginalSource(CtElement elem) {
-        SourcePosition pos = getSourcePos(elem);
-        return getOriginalSource(pos, pos);
-    }
-
-    /**
-     * Get the source file position from a CtElement, taking care that Spork sometimes stores position information as
-     * metadata to circumvent the pretty-printers reliance on positional information (e.g. when printing comments).
-     */
-    private static SourcePosition getSourcePos(CtElement elem) {
-        SourcePosition pos = (SourcePosition) elem.getMetadata(PrinterPreprocessor.POSITION_KEY);
-        if (pos == null) {
-            pos = elem.getPosition();
-        }
-        assert pos != null && pos != SourcePosition.NOPOSITION;
-
-        return pos;
-    }
-
-    private static String getOriginalSource(SourcePosition start, SourcePosition end) {
-        byte[] source = start.getCompilationUnit().getOriginalSourceCode().getBytes(Charset.defaultCharset());
-        return getOriginalSource(start, end, source);
-    }
-
-    /**
-     * Get the original source code fragment starting at start and ending at end, including indentation if start is
-     * the first element on its source code line.
-     *
-     * @param start            The source position of the first element.
-     * @param end              The source position of the last element.
-     * @return The source code fragment starting at start and ending at end, including leading indentation.
-     * @throws IOException
-     */
-    private static String getOriginalSource(SourcePosition start, SourcePosition end, byte[] source) {
-        int startByte = precededByIndentation(source, start) ?
-                getLineStartByte(start) : start.getSourceStart();
-        int endByte = end.getSourceEnd();
-
-        if (source[startByte] == 32) {
-            startByte++;
-            endByte++;
-        }
-
-        byte[] content = Arrays.copyOfRange(source, startByte, endByte + 1);
-
-        return new String(content, Charset.defaultCharset());
-    }
-
-    private static boolean precededByIndentation(byte[] source, SourcePosition pos) {
-        int lineStartByte = getLineStartByte(pos);
-
-        for (int i = lineStartByte; i < pos.getSourceStart(); i++) {
-            if (source[i] != 32) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private static int getTabCount(CtElement elem) {
-        SourcePosition pos = getSourcePos(elem);
-        byte[] fileBytes = pos.getCompilationUnit().getOriginalSourceCode().getBytes(Charset.defaultCharset());
-        int count = 0;
-
-        int[] lineSepPositions = pos.getCompilationUnit().getLineSeparatorPositions();
-        int current = 0;
-        while (current < lineSepPositions.length && lineSepPositions[current] > pos.getSourceStart())
-            current++;
-
-
-        while (current + count < pos.getSourceStart()) {
-            byte b = fileBytes[current + count];
-            if (b != 32) {
-                break;
-            }
-            ++count;
-        }
-        return count;
-    }
-
-    private static int getLineStartByte(SourcePosition pos) {
-        if (pos.getLine() == 1)
-            return 0;
-
-        int sourceStart = pos.getSourceStart() - 1;
-
-        int[] lineSepPositions = pos.getCompilationUnit().getLineSeparatorPositions();
-        int current = 0;
-        while (lineSepPositions[current] > sourceStart)
-            current++;
-
-        return lineSepPositions[current];
     }
 
     private class SporkPrinterHelper extends PrinterHelper {
@@ -313,22 +179,26 @@ public final class SporkPrettyPrinter extends DefaultJavaPrettyPrinter {
             return this;
         }
 
-        public SporkPrinterHelper writeSniperContent(String s, int tabCount) {
+        /**
+         * Write raw source code, attempting to honor indentation.
+         */
+        public SporkPrinterHelper writeRawSourceCode(String s, int indentationCount) {
             String[] lines = s.split("\n");
             if (lines.length == 1) {
-                return write(s);
+                super.write(s);
+                return this;
             }
 
             write(lines[0]);
 
             int initialTabCount = getTabCount();
-            setTabCount(tabCount / 4);
+            setTabCount(indentationCount / env.getTabulationSize());
 
 
             for (int i = 1; i < lines.length; i++) {
                 writeln();
                 String line = lines[i];
-                write(trimIndentation(line, tabCount));
+                write(trimIndentation(line, indentationCount));
             }
             setTabCount(initialTabCount);
             return this;
@@ -336,26 +206,13 @@ public final class SporkPrettyPrinter extends DefaultJavaPrettyPrinter {
 
 
         private String trimIndentation(String s, int trimAmount) {
-            if (s.length() >= trimAmount && onlyWhitespace(s.substring(0, trimAmount))) {
+            if (s.length() >= trimAmount && isOnlyWhitespace(s.substring(0, trimAmount))) {
                 return s.substring(trimAmount);
             }
             return s;
         }
 
-        private int determineTabCount(String s) {
-            for (int i = 0; i < s.length(); i++) {
-                char c = s.charAt(i);
-                if (!Character.isWhitespace(c)) {
-                    if (c == '*') { // this is most likely a block comment, which has an extra indentation
-                        return i - 1;
-                    }
-                    return i;
-                }
-            }
-            return 0;
-        }
-
-        private boolean onlyWhitespace(String s) {
+        private boolean isOnlyWhitespace(String s) {
             for (char c : s.toCharArray()) {
                 if (!Character.isWhitespace(c))
                     return false;
