cb03261a590504fdd7acb3fdf6587d28ab2905b2
slarse <slarse@kth.se>
2020-05-18 12:18:13 +0200

    Add test case for parallel add of try/catch

---
+++ src/test/resources/clean/both_modified/both_add_try/Expected.java
@@ -0,0 +1,9 @@
+class Cls {
+    public static void main(String[] args) {
+        try {
+            System.out.println("Hello, there");
+        } catch (IllegalArgumentException e) {
+            // pass
+        }
+    }
+}
---
+++ src/test/resources/clean/both_modified/both_add_try/Right.java
@@ -0,0 +1,9 @@
+class Cls {
+    public static void main(String[] args) {
+        try {
+            System.out.println("Hello");
+        } catch (IllegalArgumentException e) {
+            // pass
+        }
+    }
+}
