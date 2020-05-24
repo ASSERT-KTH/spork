48c8e78a46c9d53fbbdfc9a6262ff6e533c0fe8f
slarse <slarse@kth.se>
2020-05-24 15:43:32 +0200

    [docs] Document the SpoonNode interface
    [skip ci]

--- src/main/java/se/kth/spork/spoon/wrappers/SpoonNode.java
+++ src/main/java/se/kth/spork/spoon/wrappers/SpoonNode.java
@@ -6,16 +6,47 @@ import spoon.reflect.declaration.CtElement;
 
 import java.util.List;
 
+/**
+ * Interface describing a Spoon node as used in Spork. All concrete (i.e. non-virtual) {@link SpoonNode}s wrap a Spoon
+ * {@link CtElement}. All nodes but list edges have at least the start/end of child list virtual nodes.
+ *
+ * @author Simon Larsén
+ */
 public interface SpoonNode extends ListNode {
+
+    /**
+     * @return The element wrapped by this node. Only legal to call on concrete nodes.
+     * @throws UnsupportedOperationException If the node is not concrete.
+     */
     CtElement getElement();
 
+    /**
+     * @return The parent of this node.
+     * @throws UnsupportedOperationException If called on the virtual root.
+     */
     SpoonNode getParent();
 
+    /**
+     * @return The revision this node was created from.
+     * @throws UnsupportedOperationException If called on the virtual root.
+     */
     Revision getRevision();
 
+    /**
+     * @return All virtual children belonging to this node.
+     * @throws UnsupportedOperationException If called on a list edge.
+     */
     List<SpoonNode> getVirtualNodes();
 
+    /**
+     * @return The start of this node's child list.
+     * @throws UnsupportedOperationException If called on a list edge.
+     */
     SpoonNode getStartOfChildList();
 
+    /**
+     * @return The end of this node's child list.
+     * @throws UnsupportedOperationException If called on a list edge.
+     */
     SpoonNode getEndOfChildList();
 }
