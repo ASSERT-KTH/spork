111eb31bd058c3b34071c71dd4f61e5e3f659e5c
Simon Larsén <slarse@kth.se>
2020-07-04 19:54:55 +0200

    [fix] Fix false positive conflict in line-based merge on pure addition (#145)

---
+++ src/test/resources/clean/both_modified/add_file_header/Left.java
@@ -0,0 +1,10 @@
+package se.kth.spork;
+
+/**
+ * This is the main class :)
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
