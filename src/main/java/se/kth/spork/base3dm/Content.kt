ee0ffcf68874fbc82327ad4b33c6741d91aa3e1c
Simon Larsén <slarse@kth.se>
2020-07-05 11:24:35 +0200

    Convert Content.java to Kotlin

---
+++ src/main/java/se/kth/spork/base3dm/Content.kt
@@ -0,0 +1,23 @@
+package se.kth.spork.base3dm
+
+import java.util.*
+
+/**
+ * Container for a tree node's content. The content value belongs to the predecessor of the context PCS.
+ *
+ * @param context The context of this content. The value is associated with the predecessor of the context.
+ * @param value The value of the this content.
+ */
+data class Content<T : ListNode?, V>(val context: Pcs<T>, val value: V, val revision: Revision) {
+    override fun equals(o: Any?): Boolean {
+        if (this === o) return true
+        if (o == null || javaClass != o.javaClass) return false
+        val content = o as Content<*, *>
+        return context == content.context &&
+                value == content.value
+    }
+
+    override fun hashCode(): Int {
+        return Objects.hash(context, value)
+    }
+}
