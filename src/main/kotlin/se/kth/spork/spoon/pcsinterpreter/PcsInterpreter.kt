d3ffda6fd5c8a8943460222268badb882c71b75f
Simon Larsén <slarse@slar.se>
2021-03-19 14:58:20 +0100

    Make fromMergedPcs a top-level function

--- src/main/kotlin/se/kth/spork/spoon/pcsinterpreter/PcsInterpreter.kt
+++ src/main/kotlin/se/kth/spork/spoon/pcsinterpreter/PcsInterpreter.kt
@@ -9,42 +9,35 @@ import se.kth.spork.spoon.conflict.StructuralConflictHandler
 import se.kth.spork.spoon.conflict.ContentConflictHandler
 
 /**
- * Class for interpreting a merged PCS structure into a Spoon tree.
+ * Convert a merged PCS structure into a Spoon tree.
  *
- * @author Simon Larsén
+ * @param delta The merged change set.
+ * @param baseLeft A tree matching between the base revision and the left revision.
+ * @param baseRight A tree matching between the base revision and the right revision.
+ * @param structuralConflictHandlers A potentially empty list of structural conflict handlers.
+ * @return A pair on the form (tree, numConflicts).
  */
-object PcsInterpreter {
-    /**
-     * Convert a merged PCS structure into a Spoon tree.
-     *
-     * @param delta The merged change set.
-     * @param baseLeft A tree matching between the base revision and the left revision.
-     * @param baseRight A tree matching between the base revision and the right revision.
-     * @param structuralConflictHandlers A potentially empty list of structural conflict handlers.
-     * @return A pair on the form (tree, numConflicts).
-     */
-    fun fromMergedPcs(
-        delta: ChangeSet<SpoonNode, RoledValues>,
-        baseLeft: SpoonMapping,
-        baseRight: SpoonMapping,
-        structuralConflictHandlers: List<StructuralConflictHandler>,
-        contentConflictHandlers: List<ContentConflictHandler>
-    ): Pair<CtElement?, Int> {
-        val sporkTreeBuilder = SporkTreeBuilder(delta, baseLeft, baseRight, structuralConflictHandlers)
-        val sporkTreeRoot = sporkTreeBuilder.buildTree()
+fun fromMergedPcs(
+    delta: ChangeSet<SpoonNode, RoledValues>,
+    baseLeft: SpoonMapping,
+    baseRight: SpoonMapping,
+    structuralConflictHandlers: List<StructuralConflictHandler>,
+    contentConflictHandlers: List<ContentConflictHandler>
+): Pair<CtElement?, Int> {
+    val sporkTreeBuilder = SporkTreeBuilder(delta, baseLeft, baseRight, structuralConflictHandlers)
+    val sporkTreeRoot = sporkTreeBuilder.buildTree()
 
-        // this is a bit of a hack, get any used environment such that the SpoonTreeBuilder can copy environment details
-        val oldEnv = sporkTreeRoot
-            .children[0]
-            .node
-            .element
-            .factory
-            .environment
-        val spoonTreeBuilder = SpoonTreeBuilder(baseLeft, baseRight, oldEnv, contentConflictHandlers)
-        val spoonTreeRoot = spoonTreeBuilder.build(sporkTreeRoot)
-        return Pair(
-            spoonTreeRoot,
-            sporkTreeBuilder.numStructuralConflicts() + spoonTreeBuilder.numContentConflicts()
-        )
-    }
+    // this is a bit of a hack, get any used environment such that the SpoonTreeBuilder can copy environment details
+    val oldEnv = sporkTreeRoot
+        .children[0]
+        .node
+        .element
+        .factory
+        .environment
+    val spoonTreeBuilder = SpoonTreeBuilder(baseLeft, baseRight, oldEnv, contentConflictHandlers)
+    val spoonTreeRoot = spoonTreeBuilder.build(sporkTreeRoot)
+    return Pair(
+        spoonTreeRoot,
+        sporkTreeBuilder.numStructuralConflicts() + spoonTreeBuilder.numContentConflicts()
+    )
 }
